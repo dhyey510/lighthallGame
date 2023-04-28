8dc33600f9af85180f29f2ed8e09697d295f9e86
dhyey870 <dhyey870@gmail.com>
2023-04-28 14:47:33 -0700

    final commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.static(__dirname + "/public"));
 
 //Authentication page
 app.get("/", (req, res) => {
-  res.render("homepage.ejs", { error: null });
+  res.render("homePage.ejs", { error: null });
 });
 
 //Game Routes
